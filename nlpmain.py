7115527966334f65496ac081655ef5a266d9e054
IvanAntonioIbarra <ivan.ibarra@mexroll.com.mx>
2018-04-24 08:45:45 -0600

    Primer borrado de tokenizador

---
+++ nlpmain.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+	sentence = "Se dirigió sin dilación hacia el fondo de la cueva Encontró un hueso de gamo. Todavía tenía algo de carne adherida. Empezó a triturarlo confruición"
+	#Tokenization
+	tokens = sentence.split(" ")
+	print(tokens)
